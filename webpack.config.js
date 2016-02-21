abc5bc22ad3a25d24059fd8e823e520557b52a46
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-02-21 02:19:42 -0800

    Create base project with angular

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const PROD = (process.env.NODE_ENV && process.env.NODE_ENV === 'production');
+
+const conf = {
+  devtool: 'source-map',
+  entry  : {
+    main: `${__dirname}/src/main.js`,
+  },
+  output: {
+    path    : `${__dirname}/dist`,
+    filename: '[name].bundle.[hash].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          plugins: ['transform-decorators-legacy' ],
+          presets: ['es2015']
+        },
+      },
+      {
+        test  : /\.scss$/,
+        loader: (PROD) ? (['style', 'css', 'sass']) : (ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap', 'sass-loader')),
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      minify  : (PROD) ? ({ removeComments: true, collapseWhitespace: true, minifyJS: true }) : (false),
+    }),
+  ],
+};
+
+if (PROD) {
+  conf.plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
+} else {
+  conf.plugins.push(new ExtractTextPlugin('styles.css'));
+}
+
+module.exports = conf;
