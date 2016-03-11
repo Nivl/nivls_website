681f8c443fef222568799433a12e3cfe48bd2964
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-03-11 00:16:53 -0800

    Fix MDL on dom changes

--- src/main.js
+++ src/main.js
@@ -5,3 +5,24 @@ import { bootstrap }    from 'angular2/platform/browser';
 import { AppComponent } from './app/components/app/app';
 
 bootstrap(AppComponent);
+
+// Reload MDL on dom changes
+const observer = new MutationObserver((mutations) => {
+  let upgrade = false;
+
+  for (let i = 0; i < mutations.length; i++) {
+    if (mutations[i].addedNodes.length > 0) {
+      upgrade = true;
+      break;
+    }
+  }
+
+  if (upgrade && window.componentHandler) {
+    window.componentHandler.upgradeDom();
+  }
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree  : true,
+});
