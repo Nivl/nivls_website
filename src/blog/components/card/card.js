75e3ef1eea16a6214d41426b2843bfbb858dd8f4
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-03-15 02:07:51 -0700

    Add link on article list

--- src/blog/components/card/card.js
+++ src/blog/components/card/card.js
@@ -1,18 +1,16 @@
 import { Component } from 'angular2/core';
+import { ROUTER_DIRECTIVES, Router } from 'angular2/router';
 
 import './card.scss';
 import template from 'html!./card.html';
 
 @Component({
-  selector: 'ml-blog-card',
-  inputs  : ['article'],
+  selector  : 'ml-blog-card',
+  directives: [ROUTER_DIRECTIVES],
+  inputs    : ['article'],
   template,
 })
 
 export default class BlogCardComponent {
-  article = {};
-
-  // ngOnInit() {
-  //   this.article = article;
-  // }
+  article = {}; // @Input
 }
