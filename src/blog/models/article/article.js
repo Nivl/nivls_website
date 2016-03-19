a0caf341ea8727e6c6f6121b06d1d48a0eb57cd3
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-03-19 13:42:59 -0700

    Allow creating an empty article

--- src/blog/models/article/article.js
+++ src/blog/models/article/article.js
@@ -13,21 +13,23 @@ export default class Article {
   url = '/';
 
   constructor(data, router) {
-    this.id = data.id;
-    this.title = data.title;
-    this.subtitle = data.subtitle;
-    this.description = data.description;
-    this.createdAt = moment(data.createdAt);
-    this.content = data.content;
-    this.isPublic = data.isPublic;
-    this.slug = data.slug;
+    if (data) {
+      this.id = data.id;
+      this.title = data.title;
+      this.subtitle = data.subtitle;
+      this.description = data.description;
+      this.createdAt = moment(data.createdAt);
+      this.content = data.content;
+      this.isPublic = data.isPublic;
+      this.slug = data.slug;
 
-    this.url = router.generate(['BlogArticle', {
-      day  : this.createdAt.date() + 1,
-      month: this.createdAt.month() + 1,
-      year : this.createdAt.year(),
-      slug : this.slug,
-    }]).toUrlPath();
+      this.url = router.generate(['BlogArticle', {
+        day  : this.createdAt.date() + 1,
+        month: this.createdAt.month() + 1,
+        year : this.createdAt.year(),
+        slug : this.slug,
+      }]).toUrlPath();
+    }
   }
 
   /**
