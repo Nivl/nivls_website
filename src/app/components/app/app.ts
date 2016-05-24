93d761432a1a959ae55d300f9395469f1b296f05
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-05-23 17:58:34 -0700

    Add material design for the menu

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -1,6 +1,12 @@
 import {Component} from '@angular/core';
 import {Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdAnchor } from '@angular2-material/button';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+
 import BlogListingComponent from '../../../blog/components/listing/listing';
 import AboutComponent from '../../../about/components/about/about';
 import ProjectListingComponent from '../../../projects/components/listing/listing';
@@ -8,10 +14,17 @@ import UserLoginComponent from '../../../users/components/login/login';
 
 @Component({
   selector: 'ml-app',
-  directives: [...ROUTER_DIRECTIVES],
-  providers: [ROUTER_PROVIDERS],
   styles: [require('normalize.css'), require('./app.scss')],
   template: require('./app.html'),
+  providers: [ROUTER_PROVIDERS, MdIconRegistry],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MdIcon,
+    MdAnchor,
+    MdToolbar,
+  ],
 })
 @Routes([
   { path: '/about', component: AboutComponent },
